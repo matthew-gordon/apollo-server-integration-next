dd907a1c686550125bebe04ae111475d1d5e43a3
matthew-gordon <m.gordon5525@gmail.com>
2022-09-21 15:21:45 -0600

    [WIP] add integration test suite intial pass

---
+++ src/__tests__/integration.test.ts
@@ -0,0 +1,63 @@
+import { ApolloServer, ApolloServerOptions, BaseContext } from '@apollo/server';
+import {
+  CreateServerForIntegrationTestsOptions,
+  defineIntegrationTestSuite,
+} from '@apollo/server-integration-testsuite';
+import { createServer } from 'http';
+import type { NextApiHandler } from 'next';
+import { urlForHttpServer } from '../utils';
+import { apiResolver } from 'next/dist/server/api-utils/node';
+import { parse as parseUrl } from 'url';
+import { startServerAndCreateNextHandler } from '..';
+
+describe('nextjsHandler', () => {
+  defineIntegrationTestSuite(
+    async function (
+      serverOptions: ApolloServerOptions<BaseContext>,
+      testOptions?: CreateServerForIntegrationTestsOptions,
+    ) {
+      const httpServer = createServer();
+      const server = new ApolloServer({
+        ...serverOptions,
+      });
+
+      const handler: NextApiHandler = testOptions
+        ? startServerAndCreateNextHandler(server, testOptions)
+        : startServerAndCreateNextHandler(server);
+
+      const apiPreviewPropsMock = {
+        previewModeId: 'id',
+        previewModeEncryptionKey: 'key',
+        previewModeSigningKey: 'key',
+      };
+
+      httpServer.addListener('request', (req, res) =>
+        apiResolver(
+          req,
+          res,
+          { ...parseUrl(req.url || '', true).query },
+          handler,
+          apiPreviewPropsMock,
+          true,
+        ),
+      );
+
+      await new Promise<void>((resolve) => {
+        httpServer.listen({ port: 0 }, resolve);
+      });
+
+      return {
+        server,
+        url: urlForHttpServer(httpServer),
+        async extraCleanup() {
+          await new Promise<void>((resolve) => {
+            httpServer.close(() => resolve());
+          });
+        },
+      };
+    },
+    {
+      serverIsStartedInBackground: true,
+    },
+  );
+});
